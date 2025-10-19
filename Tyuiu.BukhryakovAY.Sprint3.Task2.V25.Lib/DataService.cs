534d669dbab87e959eaf201c75263aa366f8ef96
Артем Бухряков <artembuhryakov332@gmail.com>
2025-10-19 18:48:40 +0500

    Task2

--- Tyuiu.BukhryakovAY.Sprint3.Task2.V25.Lib/DataService.cs
+++ Tyuiu.BukhryakovAY.Sprint3.Task2.V25.Lib/DataService.cs
@@ -1,17 +1,16 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint3;
-namespace Tyuiu.BukhryakovAY.Sprint3.Task2.V25.Lib
+﻿using tyuiu.cources.programming.interfaces.Sprint3; 
+namespace Tyuiu.BukhryakovAY.Sprint3.Task2.V25.Lib;
+
+public class DataService : ISprint3Task2V25
 {
-    public class DataService : ISprint3Task2V25
+    public double GetSumSeries(int value, int startValue, int stopValue)
     {
-        public double GetSumSeries(int value, int startValue, int stopValue)
+        double sumSeries = 0;
+        do
         {
-            double sumSeries = 0;
-            do
-            {
-                sumSeries = sumSeries + Math.Pow(4 / Math.Pow(startValue, value), 2);
-                startValue++;
-            } while (startValue <= stopValue);
-            return Math.Round(sumSeries, 3);
-        }
+            sumSeries = sumSeries + Math.Pow(4 / Math.Pow(startValue, value), 2);
+            startValue++;
+        } while (startValue <= stopValue);
+        return Math.Round(sumSeries, 3);
     }
 }
